356ab4bd430422dfa7711f1eae3df2927d10f2d5
Jin Park <jin+github@jinpark.net>
2015-12-06 00:52:23 +0900

    use usergroups for volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #! /usr/bin/env bash
 
-chown -R sopel:sopel /sopel
+TARGET_GID=$(stat -c "%g" /sopel)
+EXISTS=$(cat /etc/group | grep $TARGET_GID | wc -l)
+
+# Create new group using target GID and add sopel user
+if [ $EXISTS == "0" ]; then
+  groupadd -g $TARGET_GID tempgroup
+  usermod -a -G tempgroup sopel
+else
+  # GID exists, find group name and add
+  GROUP=$(getent group $TARGET_GID | cut -d: -f1)
+  usermod -a -G $GROUP sopel
+fi
 
 exec sopel -c /sopel/default.cfg
